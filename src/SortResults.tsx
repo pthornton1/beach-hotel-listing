b0e7522039fbefcad1f640250019d8ea73a8ec7c
Philip Thornton <philipthornton198@gmail.com>
2025-03-23 21:26:55 +0000

    set background size to contain

--- src/SortResults.tsx
+++ src/SortResults.tsx
@@ -14,44 +14,22 @@ function SortResults({applyUserSort}:sortResultProps) {
     };
 
     return (
-        // <div className="d-grid gap-2">
-        //     <Container>
-        //         <Col>
-        //             <Row>
-        //                 <Button variant={sort === 'alphabetically' ? 'primary' : 'secondary'} size="lg" onClick={() => changeSort('alphabetically')}>
-        //                     sort <b>alphabetically</b> 🔤
-        //                 </Button>
-        //             </Row>
-        //             <Row>
-        //                 <Button variant={sort === 'price' ? 'primary' : 'secondary'} size="lg" onClick={() => changeSort('price')}>
-        //                     sort by <b>price</b> 💵
-        //                 </Button>
-        //             </Row>
-        //             <Row>
-        //                 <Button variant={sort === 'rating' ? 'primary' : 'secondary'} size="lg" onClick={() => changeSort('rating')}>
-        //                     Sort by <b>star rating</b> ⭐️
-        //                 </Button>
-        //             </Row>
-        //         </Col>
-        //     </Container>
-        // </div>
+        <Container className='pb-4'>
+            <div className="d-grid gap-1">
 
-<Container className='pb-4'>
-<div className="d-grid gap-1">
+                        <Button variant={sort === 'alphabetically' ? 'primary' : 'secondary'} size="lg" onClick={() => changeSort('alphabetically')}>
+                            sort <b>alphabetically</b> 🔤
+                        </Button>
 
-            <Button variant={sort === 'alphabetically' ? 'primary' : 'secondary'} size="lg" onClick={() => changeSort('alphabetically')}>
-                sort <b>alphabetically</b> 🔤
-            </Button>
+                        <Button variant={sort === 'price' ? 'primary' : 'secondary'} size="lg" onClick={() => changeSort('price')}>
+                            sort by <b>price</b> 💵
+                        </Button>
 
-            <Button variant={sort === 'price' ? 'primary' : 'secondary'} size="lg" onClick={() => changeSort('price')}>
-                sort by <b>price</b> 💵
-            </Button>
-
-            <Button variant={sort === 'rating' ? 'primary' : 'secondary'} size="lg" onClick={() => changeSort('rating')}>
-                Sort by <b>star rating</b> ⭐️
-            </Button>
-</div>
-</Container>
+                        <Button variant={sort === 'rating' ? 'primary' : 'secondary'} size="lg" onClick={() => changeSort('rating')}>
+                            Sort by <b>star rating</b> ⭐️
+                        </Button>
+            </div>
+        </Container>
     )
 
 };
