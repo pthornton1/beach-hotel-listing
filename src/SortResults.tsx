61895c351a725c987726aec0484030961c734741
Philip Thornton <philipthornton198@gmail.com>
2025-03-23 13:07:35 +0000

    create sortResults component

---
+++ src/SortResults.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+type sortApplied = "alphabetically" | "price" | "rating";
+type props = {
+    sortHotels: (by:sortApplied) => void;
+  };
+
+function SortResults({sortHotels}:props) {
+    const [sort, setSort] = useState<sortApplied>('alphabetically');
+
+    function changeSort(changeTo:sortApplied) {
+        setSort(changeTo)
+        sortHotels(changeTo)
+    };
+
+    return (
+        <div className="sortResults">
+            <button onClick={() => changeSort('price')}>Sort by Price</button>
+        </div>
+    )
+
+}
