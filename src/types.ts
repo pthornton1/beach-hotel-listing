1cc732d8cd78caede813be9ccaa604b2d08459db
Philip Thornton <philipthornton198@gmail.com>
2025-03-23 14:37:23 +0000

    create types.ts

---
+++ src/types.ts
@@ -0,0 +1,42 @@
+export interface HotelImage {
+    url: string;
+    description: string;
+  }
+  
+  export interface Resort {
+    id: string;
+    name: string;
+    regionName: string;
+    countryName: string;
+    starRating: number;
+    overview: string;
+    image: HotelImage;
+  }
+  
+  export interface FlightDetails {
+    departureAirport: string;
+    departureDate: string; // ISO string
+  }
+  
+  export interface Party {
+    adults: number;
+    children?: number;
+    infants?: number;
+  }
+  
+  export interface Price {
+    amount: number;
+    currency: string;
+  }
+  
+  export interface BookingDetails {
+    party: Party;
+    lengthOfStay: number;
+    price: Price;
+  }
+  
+  export interface Hotel {
+    resort: Resort;
+    flightDetails: FlightDetails;
+    bookingDetails: BookingDetails;
+  }
