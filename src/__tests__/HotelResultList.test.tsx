ed4f7797702b34bc80eeaef5764db3cacdb4aa2d
Philip Thornton <philipthornton198@gmail.com>
2025-03-24 16:31:31 +0000

    add tests for sort functionality

--- src/__tests__/HotelResultList.test.tsx
+++ src/__tests__/HotelResultList.test.tsx
@@ -3,11 +3,12 @@ import { render, screen } from '@testing-library/react';
 import HotelResultList from '../components/HotelResultList';
 
 // Test needs to be async as laoding data is async
-test('renders learn react link', async () => {
+test('renders list of hotels when passed valid url', async () => {
   
   // Arrange
+  // Assume this is a test url that gives back known data and is different to production
   const testUrl = "https://static.onthebeach.co.uk/fe-code-test/data.json"
-  
+
   // Act 
   render(<HotelResultList url={testUrl}/>);
   const item = await screen.findByText('Aguamarina Golf Hotel');
@@ -15,3 +16,23 @@ test('renders learn react link', async () => {
   // Assert 
   expect(item).toBeInTheDocument();
 });
+
+test('when url is invalid, give the user an error', async () => {
+  
+  // Arrange
+  const testUrl = ""
+
+  // Mock the fetch to give a network error
+  global.fetch = jest.fn().mockRejectedValue(new Error('Network Error'));
+
+
+  // Act 
+  render(<HotelResultList url={testUrl}/>);
+  const item = await screen.findByText(/Error fetching hotels/i);
+
+  // Assert 
+  expect(item).toBeInTheDocument();
+});
+
+
+
---
+++ src/test_helper/testHotels.ts
@@ -0,0 +1,80 @@
+export const testHotels =[
+    {
+      "resort": {
+        "id": "7dd27e42-2b5c-4237-86ac-97c26f72eb5b",
+        "name": "Iberostar Grand Salome",
+        "regionName": "Costa Adeje",
+        "countryName": "Tenerife",
+        "starRating": 5,
+        "overview": "The Iberostar Grand Salomehas an exceptional location in the south of Tenrife, overlooking the Atlantic Ocean. It is situated between the Golf del Sur and the Amarillo Golf Courses, and is an ideal hotel for families couples and groups who are looking for a holiday full of sport, sun and sea.",
+        "image": {
+          "url": "https://static.onthebeach.co.uk/fe-code-test/hotel-image-1.jpg",
+          "description": "A tranquil resort swimming pool with clear blue water, surrounded by two-story villas with terracotta roofs under a bright blue sky."
+        }
+      },
+      "flightDetails": {
+        "departureAirport": "East Midlands",
+        "departureDate": "2030-07-03T00:00:00.000Z"
+      },
+      "bookingDetails": {
+        "party": { "adults": 2, "children": 2, "infants": 1 },
+        "lengthOfStay": 7,
+        "price": {
+          "amount": 1136.5,
+          "currency": "GBP"
+        }
+      }
+    },
+    {
+      "resort": {
+        "id": "f0ec3974-fb4e-4fb5-b964-2ac8a2e2c1a4",
+        "name": "Aguamarina Golf Hotel",
+        "regionName": "Costa Adeje",
+        "countryName": "Tenerife",
+        "starRating": 4,
+        "overview": "The Hotel Aguamarina Golf has an exceptional location in the south of Tenerife, overlooking the Atlantic Ocean. It is situated between the Golf del Sur and the Amarillo Golf courses, and is an ideal hotel for families, couples and groups who are looking for a holiday full of sport, sun and sea.",
+        "image": {
+          "url": "https://static.onthebeach.co.uk/fe-code-test/hotel-image-2.jpg",
+          "description": "A bustling resort pool complex with palm trees, white buildings with red roofing, and guests enjoying sunbeds under blue parasols."
+        }
+      },
+      "flightDetails": {
+        "departureAirport": "Liverpool",
+        "departureDate": "2030-05-27T00:00:00.000Z"
+      },
+      "bookingDetails": {
+        "party": { "adults": 2, "children": 1 },
+        "lengthOfStay": 7,
+        "price": {
+          "amount": 696.8,
+          "currency": "GBP"
+        }
+      }
+    },
+    {
+      "resort": {
+        "id": "674b4c76-4527-4a5b-a07a-717e3c4891ed",
+        "name": "Las Piramides Resort",
+        "regionName": "Costa Adeje",
+        "countryName": "Tenerife",
+        "starRating": 3,
+        "overview": "What do you get when you combine comfortable rooms, located in the heart of the action and all for a budget-friendly price? A very, very good holiday. That's what. Hugely popular with the young and lively crowd, the Las Piramides Resort offers simple, budget accommodation for those looking to be right amongst it. At just 350m from the famous Veronicas Strip and 50m from the nearest shops, bars and restaurants, the property is perfectly located for nights out on the town. There’s also an outdoor swimming pool that is perfect for a chill-out session too.",
+        "image": {
+          "url": "https://static.onthebeach.co.uk/fe-code-test/hotel-image-3.jpg",
+          "description": "A serene poolside view at dusk, with calm waters reflecting palm trees and blue sun loungers, bordered by a beachfront promenade."
+        }
+      },
+      "flightDetails": {
+        "departureAirport": "Manchester",
+        "departureDate": "2030-07-03T00:00:00.000Z"
+      },
+      "bookingDetails": {
+        "party": { "adults": 2, "children": 2 },
+        "lengthOfStay": 7,
+        "price": {
+          "amount": 499.99,
+          "currency": "GBP"
+        }
+      }
+    }
+  ]
