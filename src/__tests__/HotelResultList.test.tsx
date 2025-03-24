e2c1fe5d365b9321b78cedbd8216474215f777ba
Philip Thornton <philipthornton198@gmail.com>
2025-03-24 15:29:09 +0000

    intitial test

--- src/__tests__/HotelResultList.test.tsx
+++ src/__tests__/HotelResultList.test.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import HotelResultList from '../components/HotelResultList';
 
-test('renders learn react link', () => {
+test('renders learn react link', async () => {
   render(<HotelResultList />);
-  const linkElement = screen.getByText(/hello/i);
-  expect(linkElement).toBeInTheDocument();
+  // const linkElement = screen.getByText('Aguamarina Golf Hotel');
+  // expect(linkElement).toBeInTheDocument();
+  const item = await screen.findByText('Aguamarina Golf Hotel');
+  expect(item).toBeInTheDocument();
 });
