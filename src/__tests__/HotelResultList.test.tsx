2f3b63375c60c5d86e9a90b3f5531a4ddf66e90e
Philip Thornton <philipthornton198@gmail.com>
2025-03-24 15:35:34 +0000

    fix render test for HotelResultList

--- src/__tests__/HotelResultList.test.tsx
+++ src/__tests__/HotelResultList.test.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import HotelResultList from '../components/HotelResultList';
 
+// Test needs to be async as laoding data is async
 test('renders learn react link', async () => {
-  render(<HotelResultList />);
-  // const linkElement = screen.getByText('Aguamarina Golf Hotel');
-  // expect(linkElement).toBeInTheDocument();
+  
+  // Arrange
+  const testUrl = "https://static.onthebeach.co.uk/fe-code-test/data.json"
+  
+  // Act 
+  render(<HotelResultList url={testUrl}/>);
   const item = await screen.findByText('Aguamarina Golf Hotel');
+
+  // Assert 
   expect(item).toBeInTheDocument();
 });
