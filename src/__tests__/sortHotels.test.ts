2689751b2597700f949fca6f8e5eb7f82c90d1a2
Philip Thornton <philipthornton198@gmail.com>
2025-03-24 16:46:41 +0000

    finish sort hotel tests

--- src/__tests__/sortHotels.test.ts
+++ src/__tests__/sortHotels.test.ts
@@ -1,39 +1,38 @@
 import sortHotels from "../components/sortHotels";
 import { testHotels } from "../test_helper/testHotels";
   
-      describe('sortHotels function', () => {
+describe('sortHotels function', () => {
 
-        test('handels empty input', () => {
-            const sortedHotels = sortHotels([], 'alphabetically');
-            expect(sortedHotels).toEqual([]);
-          }); 
+    test('handels empty input', () => {
+        const sortedHotels = sortHotels([], 'alphabetically');
+        expect(sortedHotels).toEqual([]);
+        }); 
 
-        test('sorts hotels alphabetically', () => {
-          const sortedHotels = sortHotels(testHotels, 'alphabetically');
-          expect(sortedHotels[0].resort.name).toBe('Aguamarina Golf Hotel');
-          expect(sortedHotels[1].resort.name).toBe('Iberostar Grand Salome');
-          expect(sortedHotels[2].resort.name).toBe('Las Piramides Resort');
-        });
-      
-        test('sorts hotels by price', () => {
-          const sortedHotels = sortHotels(testHotels, 'price');
-          expect(sortedHotels[2].bookingDetails.price.amount).toBe(1136.5);
-          expect(sortedHotels[1].bookingDetails.price.amount).toBe(696.8); 
-          expect(sortedHotels[0].bookingDetails.price.amount).toBe(499.99); 
-        });
-      
-        test('sorts hotels by rating', () => {
-          const sortedHotels = sortHotels(testHotels, 'rating');
-          expect(sortedHotels[0].resort.starRating).toBe(5); 
-          expect(sortedHotels[1].resort.starRating).toBe(4); 
-          expect(sortedHotels[2].resort.starRating).toBe(3); 
-        });
-      
-        test('returns hotels by price no sorting is specified', () => {
-          const sortedHotels = sortHotels(testHotels);
-          expect(sortedHotels[2].bookingDetails.price.amount).toBe(1136.5);
-          expect(sortedHotels[1].bookingDetails.price.amount).toBe(696.8); 
-          expect(sortedHotels[0].bookingDetails.price.amount).toBe(499.99); 
-        });
-      });
-      
+    test('sorts hotels alphabetically', () => {
+        const sortedHotels = sortHotels(testHotels, 'alphabetically');
+        expect(sortedHotels[0].resort.name).toBe('Aguamarina Golf Hotel');
+        expect(sortedHotels[1].resort.name).toBe('Iberostar Grand Salome');
+        expect(sortedHotels[2].resort.name).toBe('Las Piramides Resort');
+    });
+
+    test('sorts hotels by price', () => {
+        const sortedHotels = sortHotels(testHotels, 'price');
+        expect(sortedHotels[2].bookingDetails.price.amount).toBe(1136.5);
+        expect(sortedHotels[1].bookingDetails.price.amount).toBe(696.8); 
+        expect(sortedHotels[0].bookingDetails.price.amount).toBe(499.99); 
+    });
+
+    test('sorts hotels by rating', () => {
+        const sortedHotels = sortHotels(testHotels, 'rating');
+        expect(sortedHotels[0].resort.starRating).toBe(5); 
+        expect(sortedHotels[1].resort.starRating).toBe(4); 
+        expect(sortedHotels[2].resort.starRating).toBe(3); 
+    });
+
+    test('returns hotels by price no sorting is specified', () => {
+        const sortedHotels = sortHotels(testHotels);
+        expect(sortedHotels[2].bookingDetails.price.amount).toBe(1136.5);
+        expect(sortedHotels[1].bookingDetails.price.amount).toBe(696.8); 
+        expect(sortedHotels[0].bookingDetails.price.amount).toBe(499.99); 
+    });
+});
